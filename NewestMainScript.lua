ad741aba83f1d5d52f6a6d205dc6c4c480add379
Maanaaaa <markfokin00@gmail.com>
2024-11-17 12:44:30 +0200

    fix 2

--- NewestMainScript.lua
+++ NewestMainScript.lua
@@ -1,5 +1,6 @@
 local Functions = {}
 _G.Functions = Functions -- for KeySystemLibrary.lua
+local httprequest = (request and http and http.request or http_request or fluxus and fluxus.request)
 
 function Functions:RunFile(filepath)
     if isfile("NewMana/" .. filepath) then
@@ -17,7 +18,7 @@ local Window = KeySystemLibrary:Create({
     Key = "$Dy71b0o4a(*g)!g%",
     Callback = function(callback)
         if callback then
-            Functions:RunFile("MainScript.lua")
+            Functions:RunFile("NewMainScript.lua")
         else
             print("[ManaV2ForRoblox/NewestMainScript.lua]: Something went wrong, try again or message the developer on discord - @mankacoder.")
         end
